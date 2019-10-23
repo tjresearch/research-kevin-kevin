e29fe045b92389e9196cace616eb63c17b3d36a8
kevinchung888 <kevinchung888@yahoo.com>
2019-10-23 09:43:54 -0400

    started lattice point detection

--- board/board_detection/board_locator.py
+++ board/board_detection/board_locator.py
@@ -1,89 +1,18 @@
-import cv2
 import numpy as np
 import itertools
 import utils
 import math
 import line_detection
-from sklearn.cluster import dbscan
-from matplotlib import pyplot as plt
 
 
-def line_weight(line, canny):
-	m = -np.cos(line[1]) / np.sin(line[1])
-	b = line[0] / np.sin(line[1])
-	endpoints = [(0, b), (canny.shape[1] - 1, m * (canny.shape[1] - 1) + b),
-				 (-b / m, 0), ((canny.shape[0] - 1 - b) / m, canny.shape[0] - 1)]
-	endpoints = [p for p in endpoints if 0 <= p[0] < canny.shape[1] and 0 <= p[1] < canny.shape[0]]
-	mag = int(np.hypot(endpoints[0][0] - endpoints[1][0], endpoints[0][1] - endpoints[1][1]))
-	xlin = np.linspace(endpoints[0][0], endpoints[1][0], mag)
-	ylin = np.linspace(endpoints[0][1], endpoints[1][1], mag)
-	weight = 0
-	for i in range(mag):
-		weight += canny[int(ylin[i]), int(xlin[i])] // 255
-	return weight
-
-
-def rho_theta_distance(p1, p2):
-
-	rho_dist1 = (p1[0] - p2[0]) ** 2
-	theta_dist1 = (p1[1] - p2[1]) ** 2
-	rho_dist2 = (p1[0] + p2[0]) ** 2
-	if p1[1] < p2[1]:
-		theta_dist2 = (p1[1] - p2[1] + 1) ** 2
-	else:
-		theta_dist2 = (p2[1] - p1[1] + 1) ** 2
-	return min(np.sqrt(rho_dist1 + theta_dist1), np.sqrt(rho_dist2 + theta_dist2))
-
-
-def find_lines(img): # Will be implemented with more advanced algorithms later
-
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	gray = cv2.GaussianBlur(gray, (7, 7), 0)
-
-	canny = cv2.Canny(gray, 60, 120)
-
-	# cv2.imshow("canny", canny)
-
-	lines = cv2.HoughLines(canny, 1, np.pi/180, 90)
-
-	lines = lines[:, 0]
-
-	return filter_lines(lines)
-
-
-def find_lines_improved(img):
-	lines = line_detection.find_lines(img)
-	rho_theta_lines = []
+def separate_lines(lines):
+	hor, vert = [], []
 	for line in lines:
-		rho_theta_lines.append(utils.convert_ab_to_rho_theta(line))
-	return rho_theta_lines
-	# return filter_lines(np.array(rho_theta_lines))
-
-
-def filter_lines(lines):
-	data = lines.copy()
-
-	data[:, 0] = data[:, 0] / np.max(np.abs(data[:, 0]))
-	data[:, 1] = data[:, 1] / np.pi
-
-	indices, clusters = dbscan(data, 0.02, min_samples=1, metric=rho_theta_distance)
-
-	lines = lines[indices]
-	clusters = clusters[indices]
-	num_clusters = len(set(clusters))
-
-	firsts = [clusters.tolist().index(i) for i in range(num_clusters)]
-
-	# plt.scatter(lines[:, 0, 0], lines[:, 0, 1], c=clusters)
-	# title = "number of cluster: {}".format(num_clusters)
-	# plt.title(title)
-	# plt.xlabel("Rho")
-	# plt.ylabel("Theta")
-	# plt.show()
-
-	best_lines = [list(lines[i]) for i in firsts]
-
-	return best_lines
+		if np.pi / 4 < line[1] < 3 * np.pi / 4:
+			hor.append(line)
+		else:
+			vert.append(line)
+	return hor, vert
 
 
 def get_intersections(lines):
@@ -100,7 +29,7 @@ def get_intersections(lines):
 
 
 def find_chessboard(img):
-	lines = find_lines_improved(img)
+	lines = line_detection.find_lines_improved(img)
 
 	lattice_points = [p for p in get_intersections(lines) if 0 <= p[0] < img.shape[1] and 0 <= p[1] < img.shape[0]]
 
@@ -108,49 +37,53 @@ def find_chessboard(img):
 
 	best_board = (None, 0, math.inf)
 
-	for edges in itertools.combinations(lines, 4):
-		corners = [p for p in get_intersections(edges) if 0 <= p[0] < img.shape[1] and 0 <= p[1] < img.shape[0]]
-		if len(corners) == 4:
-			disp = img.copy()
-			corners = utils.sorted_ccw(corners)
+	horizontal, vertical = separate_lines(lines)
 
-			# for lattice_point in lattice_points:
-			# 	cv2.circle(disp, (int(lattice_point[0]), int(lattice_point[1])), 3, (0, 0, 255), 2)
-			#
-			# for corner in corners:
-			# 	cv2.circle(disp, (int(corner[0]), int(corner[1])), 3, (255, 0, 0), 2)
+	for hor in itertools.combinations(horizontal, 2):
+		for vert in itertools.combinations(vertical, 2):
+			edges = [*hor, *vert]
+			corners = [p for p in get_intersections(edges) if 0 <= p[0] < img.shape[1] and 0 <= p[1] < img.shape[0]]
+			if len(corners) == 4:
+				disp = img.copy()
+				corners = utils.sorted_ccw(corners)
 
-			square_size = 100
-			H = utils.find_homography(corners, square_size)
+				# for lattice_point in lattice_points:
+				# 	cv2.circle(disp, (int(lattice_point[0]), int(lattice_point[1])), 3, (0, 0, 255), 2)
+				#
+				# for corner in corners:
+				# 	cv2.circle(disp, (int(corner[0]), int(corner[1])), 3, (255, 0, 0), 2)
 
-			close_points = 0
-			total_dist = 0
+				square_size = 100
+				H = utils.find_homography(corners, square_size)
 
-			matched = []
+				close_points = 0
+				total_dist = 0
 
-			for i in range(9):
-				skip = False
-				for j in range(9):
-					try:
-						point = utils.inverse_warp_point((i * square_size, j * square_size), H)
-					except np.linalg.LinAlgError:
-						skip = True
-						break
-					# cv2.circle(disp, (int(point[0]), int(point[1])), 3, (0, 255, 0), 2)
-					for lattice_point in lattice_points:
-						if utils.dist(point, lattice_point) < 20 and lattice_point not in matched:
-							matched.append(lattice_point)
-							close_points += 1
-							total_dist += utils.dist(point, lattice_point)
+				matched = []
+
+				for i in range(9):
+					skip = False
+					for j in range(9):
+						try:
+							point = utils.inverse_warp_point((i * square_size, j * square_size), H)
+						except np.linalg.LinAlgError:
+							skip = True
 							break
-				if skip:
-					break
+						# cv2.circle(disp, (int(point[0]), int(point[1])), 3, (0, 255, 0), 2)
+						for lattice_point in lattice_points:
+							if utils.dist(point, lattice_point) < 20 and lattice_point not in matched:
+								matched.append(lattice_point)
+								close_points += 1
+								total_dist += utils.dist(point, lattice_point)
+								break
+					if skip:
+						break
 
-			if close_points > best_board[1] or (close_points == best_board[1] and total_dist < best_board[2]):
-				best_board = (corners, close_points, total_dist)
+				if close_points > best_board[1] or (close_points == best_board[1] and total_dist < best_board[2]):
+					best_board = (corners, close_points, total_dist)
 
-			# cv2.imshow("corners", disp)
-			# cv2.waitKey()
+				# cv2.imshow("corners", disp)
+				# cv2.waitKey()
 
 	return best_board[0]
 
--- board/board_detection/line_linking.py
+++ board/board_detection/line_linking.py
@@ -8,7 +8,7 @@ def get_line_eq(line):
 	A = np.linalg.inv(np.array([[p1[0], p1[1]], [p2[0], p2[1]]]))
 	B = np.array([[1], [1]])
 
-	return np.matmul(A, B)[:, 0].tolist()
+	return np.round(np.matmul(A, B)[:, 0].tolist(), 10)
 
 def find_intersection(l1, l2):
 	a1, b1 = get_line_eq(l1)
@@ -23,9 +23,6 @@ def find_intersection(l1, l2):
 
 	return np.matmul(A, B)[:, 0].tolist()
 
-def dist(p1, p2): #repeat?
-	return np.hypot(p1[0] - p2[0], p1[1] - p2[1])
-
 def angle(l1, l2):
 	v1 = np.array([l1[1][0] - l1[0][0], l1[1][1] - l1[0][1]])
 	v2 = np.array([l2[1][0] - l2[0][0], l2[1][1] - l2[0][1]])
@@ -37,7 +34,7 @@ def calc_gamma(l1, l2):
 	inter = find_intersection(l1, l2)
 	if inter is None:
 		return None
-	return (dist(l1[0], inter) + dist(l1[1], inter) + dist(l2[0], inter) + dist(l2[1], inter)) * np.sin(angle(l1, l2)) / 4
+	return (utils.dist(l1[0], inter) + utils.dist(l1[1], inter) + utils.dist(l2[0], inter) + utils.dist(l2[1], inter)) * np.sin(angle(l1, l2)) / 4
 
 def linkable(l1, l2, img):
 	omega = np.pi / (2 * math.pow(img.shape[0] * img.shape[1], 1/4))
@@ -49,7 +46,8 @@ def linkable(l1, l2, img):
 	gamma = calc_gamma(l1, l2)
 	if gamma is None:
 		return False
-	return (mag1 / gamma > delta) ^ (mag2 / gamma > delta)
+	else:
+		return (mag1 / gamma > delta) ^ (mag2 / gamma > delta)
 
 
 def link(lines):
