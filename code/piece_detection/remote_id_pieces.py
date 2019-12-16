f037eccc5ffdbade9d95514586fa6f443c9a97da
kfu02 <kevin.fu842@gmail.com>
2019-12-16 09:29:28 -0500

    cudnn bugfix?"

--- code/piece_detection/remote_id_pieces.py
+++ code/piece_detection/remote_id_pieces.py
@@ -9,6 +9,13 @@ piece splitting system in piece_labelling.py
 4. (if verbose) probabilities shown
 """
 
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
 import os, sys
 import time
 import numpy as np
