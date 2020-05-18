dfa8bf136dee47b4d03ea79b13ad08d52a9a7a33
kfu02 <kevin.fu842@gmail.com>
2020-05-18 15:11:51 -0400

    duplicate incr_color_contrast methods

--- code/piece_detection/square_splitter.py
+++ code/piece_detection/square_splitter.py
@@ -31,19 +31,6 @@ def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
 
 	return cv2.resize(image, dim, interpolation=inter)
 
-# https://stackoverflow.com/questions/19363293/whats-the-fastest-way-to-increase-color-image-contrast-with-opencv-in-python-c
-def increase_color_contrast(src, clim, tgs):
-	clahe = cv2.createCLAHE(clipLimit=clim, tileGridSize=tgs) #get CLAHE from normal img
-
-	lab = cv2.cvtColor(src, cv2.COLOR_BGR2LAB)  # convert from BGR to LAB color space
-	l, a, b = cv2.split(lab)  # split on 3 different channels
-
-	l2 = clahe.apply(l)  # apply CLAHE to the L-channel
-	lab = cv2.merge((l2,a,b))  # merge channels
-
-	output = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)  # convert back to BGR
-	return output
-
 """
 transform Canny edge version of chessboard
 identify possible squares w/ pieces based on # of canny pixels in square
